e7eca63a2bec2cc9e83649af094d360260e1f4dd
Vitor Escalfoni <vitor.hme@gmail.com>
2022-08-13 21:09:46 -0300

    chore: added static assets

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,12 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Vitor Escalfoni</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta
           name="description"
-          content="Vitor Escalfoni is a software engineer specialized in mobile app development who thrives on crafting great user experiences."
+          content="Vitor Escalfoni is a software engineer specialized in mobile app development with TypeScript and React Native who thrives on crafting great user experiences."
         />
       </Head>
       <Component {...pageProps} />
