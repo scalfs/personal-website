22b0b8e2dea2aeff6e746f5fd055f192ef06b343
Vitor Escalfoni <vitor.hme@gmail.com>
2022-08-13 21:36:53 -0300

    feat: organized main file

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,28 @@
+import About from 'components/About'
+import Contact from 'components/Contact'
+import Experience from 'components/Experience'
+import Footer from 'components/Footer'
 import Header from 'components/Header'
 import Hero from 'components/Hero'
 import LayoutBlur from 'components/LayoutBlur'
 
 const Main = () => (
-  <div className="min-h-screen bg-gray-50 text-zinc-900">
-    <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
-      <LayoutBlur />
+  <div>
+    <Header />
 
-      <Header />
+    <LayoutBlur type="top" />
 
+    <main className="mx-auto max-w-5xl overflow-hidden px-4 sm:px-6 sm:text-lg md:px-8">
       <Hero />
-    </div>
+
+      <About />
+
+      <Experience />
+
+      <Contact />
+    </main>
+
+    <Footer />
   </div>
 )
 
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,11 +1,14 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Main from '.'
 
-describe('', () => {
-  it('should render the colors correctly', () => {
-    const { container } = render(<Main />)
+describe('<Main />', () => {
+  it('should render correctly and have proper roles', () => {
+    render(<Main />)
 
-    expect(container.firstChild).toHaveClass('bg-gray-50')
+    expect(screen.getByRole('banner')).toBeInTheDocument()
+    expect(screen.getByRole('navigation')).toBeInTheDocument()
+    expect(screen.getByRole('main')).toBeInTheDocument()
+    expect(screen.getByRole('contentinfo')).toBeInTheDocument()
   })
 })
